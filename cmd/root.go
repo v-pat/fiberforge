d4b6b935b955916501e5e5224f5199792a468501
Vaibhav <vaibhav.pathak@pies.io>
2024-03-08 22:57:56 +0530

    fix: Added version in rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "fiberforge [command]",
-	Short: "A CLI to get started with go fiber app instantly",
-	Long:  "Fiberforge will create a backend setup or based on your input, even generate CRUD APIs for your tables/collections.",
-}
-
-func init() {
+	Use:     "fiberforge [command]",
+	Short:   "A CLI to get started with go fiber app instantly",
+	Long:    "Fiberforge will create a backend setup or based on your input, even generate CRUD APIs for your tables/collections.",
+	Version: "v0.0.7",
 }
 
 func Execute() {
